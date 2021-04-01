be68af52a3bc9a7358b2583601d69f1c70074ea4
David-Alexandre COHEN <david.alex.cohen@gmail.com>
2021-04-01 23:11:15 +0200

    test(config.js): add detailled tests for config.js

--- test/utils/config.test.js
+++ test/utils/config.test.js
@@ -1,44 +1,91 @@
-import { load, save, clear, defaultConfig } from "../src/js/utils/config";
-import { assert } from "chai";
+import { load, save, clear, defaultConfig } from "../../src/js/utils/config";
+import { expect, assert } from "chai";
 
-const spy = { req: null };
+import chrome from "sinon-chrome/extensions";
+import { match } from "sinon";
 
-const mockedChrome = {
-  storage: {
-    sync: {
-      get: (a, cb) => {
-        cb(this.config || null);
-      },
-      set: (config, cb) => {
-        this.config = config;
-        cb(this.config);
-      },
-    },
-  },
-};
 
-describe("config", () => {
-  before(function () {
-    global.chrome = mockedChrome;
+describe("utils/config", () => {
+  before(() => {
+    global.chrome = chrome;
   });
-  it("should load without error", async () => {
-    const conf = await load();
-    assert.deepEqual(conf, { isConfigured: false });
-  });
-  it("should save without error", async () => {
-    const conf = await save({ autoSyncDelay: "2000", customRootName: "customRootName" });
-    assert.deepEqual(conf, {
-      autoSyncDelay: "2000",
-      customRootName: "customRootName",
-      isConfigured: true,
-      rootBookmarkId: undefined,
-      strapiJwt: undefined,
-      strapiUrl: undefined,
+
+  describe("#load", () => {
+    before(() => {
+      // Setup empty storage
+      chrome.storage.sync.get.yields(defaultConfig);
+    });
+
+    after(() => {
+      chrome.storage.sync.get.reset();
+    });
+
+    it("should return a Promise resolving an object", async () => {
+      const promise = load();
+      const result = await promise;
+      expect(promise).to.be.a('promise');
+      expect(result).to.be.an('object');
+    });
+
+    it("should return isConfigured=false by default", async () => {
+      const result = await load();
+      expect(result).to.have.property("isConfigured").that.equal(false);
     });
   });
 
-  it("should clear without error", async () => {
-    const conf = await clear();
-    assert.deepEqual(conf, defaultConfig);
+  describe("#save", () => {
+    beforeEach(() => {
+      chrome.storage.sync.set.yields({});
+    });
+
+    afterEach(() => {
+      chrome.storage.sync.set.reset();
+    });
+
+    it("should returns a Promise", () => {
+      const result = save();
+      expect(result).to.be.a('promise');
+    });
+
+    it("should ignore unknown keys", async () => {
+      const result = save({ autoSyncDelay: "2000", helloUnknown: "world" });
+      await result;
+      expect(chrome.storage.sync.set).to.have.been.calledWithMatch(match.has("autoSyncDelay", "2000"));
+      expect(chrome.storage.sync.set).to.not.have.been.calledWithMatch(match.has("helloUnknown", "world"));
+    });
+
+    it("should accept ALL valid keys", async () => {
+      const validKeys = [
+        "autoSyncDelay",
+        "customRootName",
+        "rootBookmarkId",
+        "strapiJwt",
+        "strapiUrl",
+      ];
+      const config = validKeys.reduce((agg, key) => ({ ...agg, [key]: "value" }), {});
+      await save(config);
+      expect(chrome.storage.sync.set).to.have.been.calledWithMatch(match(config));
+    });
+  });
+
+  describe("#clear", () => {
+    beforeEach(() => {
+      chrome.storage.sync.set.yields({});
+    });
+
+    afterEach(() => {
+      chrome.storage.sync.set.reset();
+    });
+
+    it("should return a promise", () => {
+      const result = clear();
+      expect(result).to.be.a('promise');
+    });
+
+    it("should clear without error", async () => {
+      const result = clear();
+      await result;
+      expect(chrome.storage.sync.set).to.have.been.calledWith(defaultConfig);
+    });
   });
 });
